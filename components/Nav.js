9cd1e949e0244a5918ae413327bd6baa1a5b17fa
taurusilver7 <prasad20697@gmail.com>
2021-04-29 19:18:15 +0530

    navbar structure completed

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,20 @@
-import requests from "../utils/requests"
+import requests from "../utils/requests";
 
 const Nav = () => {
-    return (
-        <nav>
-            <div>
-                {Object.entries(requests).map(([key, {title, url}]) => (
-                    <h2>{title}</h2>
-                ))}
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll ">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
