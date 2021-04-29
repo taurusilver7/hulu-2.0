526479e4806c4a89e42bc1dea58d13b176f960e3
taurusilver7 <prasad20697@gmail.com>
2021-04-29 12:26:49 +0530

    home icon added

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,9 +1,10 @@
-const HeaderItem = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const HeaderItem = ({ Icon, title }) => {
+  return (
+    <div>
+      <Icon className="h-8 mb-1" />
+      <p className="tracking-widest">{title}</p>
+    </div>
+  );
+};
 
-export default HeaderItem
+export default HeaderItem;
