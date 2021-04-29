fe008fbfa411933c0f74e6eb013a86056356c075
taurusilver7 <prasad20697@gmail.com>
2021-04-29 14:10:09 +0530

    navbar function complete

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,13 @@
+import requests from "../utils/requests"
+
 const Nav = () => {
     return (
         <nav>
-            
+            <div>
+                {Object.entries(requests).map(([key, {title, url}]) => (
+                    <h2>{title}</h2>
+                ))}
+            </div>
         </nav>
     )
 }
