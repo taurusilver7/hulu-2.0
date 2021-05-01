4b0e9161062dfacdfcc50eac30c29fef65493a40
taurusilver7 <prasad20697@gmail.com>
2021-05-01 22:41:40 +0530

    server side render props results

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -18,7 +19,7 @@ export default function Home() {
       <Nav />
 
       {/* Results */}
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
@@ -28,4 +29,15 @@ export async function getServerSideProps(context) {
   const genre = context.query.genre;
 
   // req to the database.
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
