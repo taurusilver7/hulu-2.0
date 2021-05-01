b34d716e83aba3d2093b46f89343a88425f8f3a7
taurusilver7 <prasad20697@gmail.com>
2021-05-01 19:28:01 +0530

    results components added to page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
@@ -17,6 +18,8 @@ export default function Home() {
       <Nav />
 
       {/* Results */}
+      <Results />
     </div>
   );
 }
+
