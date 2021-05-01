3b878874bc8a270d81fb08eda45928bdcf6daeb0
taurusilver7 <prasad20697@gmail.com>
2021-05-01 19:19:53 +0530

    nav scrollbar effects

--- components/Nav.js
+++ components/Nav.js
@@ -2,8 +2,8 @@ import requests from "../utils/requests";
 
 const Nav = () => {
   return (
-    <nav>
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll ">
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
@@ -13,6 +13,7 @@ const Nav = () => {
           </h2>
         ))}
       </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
     </nav>
   );
 };
