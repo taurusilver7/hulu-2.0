639f84ba06a1de85383a4e56ffc327dddb02a190
taurusilver7 <prasad20697@gmail.com>
2021-05-02 12:13:15 +0530

    result thumbnail card created

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,9 +1,26 @@
-const Thumbnail = ({result}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import Image from "next/image";
 
-export default Thumbnail
+const Thumbnail = ({ result }) => {
+  //   console.log(result);
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  return (
+    <div>
+      <Image
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`
+        }
+        layout="responsive"
+        height={1080}
+        width={1920}
+      />
+      <div className="p-2">
+        <p className="truncate max-w-md">{result.overview}</p>
+        <h2>{result.title || result.original_name}</h2>
+        <p>{result.media_type && `${result.media_type} ·`} </p>
+      </div>
+    </div>
+  );
+};
+
+export default Thumbnail;
